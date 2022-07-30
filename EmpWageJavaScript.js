06b4e6ac4e2d579785d085b4b0b4ce590da6b7c3
neeladri22 <neeladri22@gmail.com>
2022-07-30 20:13:04 +0530

    Calculate Daily Employee Wage part time or full time

--- EmpWageJavaScript.js
+++ EmpWageJavaScript.js
@@ -1,6 +1,8 @@
 console.log("Welcome to emp wage program using java script");
 
 //UC1
+
+/*
 const IS_ABSENT = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 2;
@@ -11,3 +13,25 @@ if (empCheck == IS_ABSENT) {
 else {
     console.log("Employee is Present");
 }
+*/
+
+//UC2 -Calculate Daily Employee Wage part time or full time
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("EmpWage:" + empWage);
